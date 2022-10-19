27ea49cc30b4689edf76e41316d4ec5874fc2d83
Roshith P <roshith844@gmail.com>
2022-10-19 16:37:22 +0530

    Update: Add content Type

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const http = require('http');
 
 const requestListener = function (req, res) {
-  res.writeHead(200);
+  res.writeHead(200,{'Content-Type' : 'text/html'});
   res.end('Hello, World!');
 }
 
