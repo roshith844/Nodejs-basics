9f66efb63f0e041f8f7266f295e5b73dc8b0dbfb
Roshith P <roshith844@gmail.com>
2022-10-20 09:58:21 +0530

    Test dynamic routing

--- express-tutorial.js
+++ express-tutorial.js
@@ -1,10 +1,16 @@
 const express = require('express')
  const app = express()
+const id = req.query.id
  app.get('/',(req,res)=>{
      res.send('hello world')
  })
  app.get('/info', (req,res)=>{
      res.send('It is made by roshith')
  })
+ app.get('/info/:id',(req,res)=>{
+     const id = req.params.id
+     res.send('its number '+ id)
+ })
+ 
   
  app.listen(8080,(req,res)=>console.log('running'))
